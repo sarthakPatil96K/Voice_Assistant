a40bfe54515f3be7c9549e2c8b605320117245ee
[Sarthak Patil] <[Sarthakjpatil745@gmail.com ]>
2024-12-23 22:41:22 +0530

    chat Box added with buttons

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import eel
+from engine.command import*
+from engine.features import*
 
+playAssitantSound()
 eel.init("www")
 os.system('start chrome.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html',mode = None,host='localhost',block=True)
 
-# from engine.command import*
-# from engine.features import*
-# playAssitantSound()
+
 # allCommands()
--- www/main.js
+++ www/main.js
@@ -2,11 +2,45 @@ $(document).ready(function () {
     $('.text').textillate({
         loop: true,
         sync: true,
-        in:{
+        in: {
             effect: "bounceIn",
         },
-        out:{
+        out: {
             effect: "bounceOut",
         }
     })
+    // Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+    });
+
+    // Siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+
+    });
+
+    // mic button click event
+
+    $("#MicBtn").click(function () {
+        eel.playAssistantSound()
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+        eel.allCommands()()
+    });
 });
