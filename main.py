595c078591e69f13e0473e0b94870965c68409fa
[Sarthak Patil] <[Sarthakjpatil745@gmail.com ]>
2024-12-23 14:17:23 +0530

    frontend meged with python

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import eel
-from engine.command import*
-from engine.features import*
-playAssitantSound()
-allCommands()
+
+eel.init("www")
+os.system('start chrome.exe --app="http://localhost:8000/index.html"')
+eel.start('index.html',mode = None,host='localhost',block=True)
+
+# from engine.command import*
+# from engine.features import*
+# playAssitantSound()
+# allCommands()
